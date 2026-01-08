75c911dae66231a264562d05f51f6a051958af3f
ndrscalia <andrea95scalia@gmail.com>
2026-01-08 22:40:31 +0100

    fix mason and lsp for nvchad

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "marksman", "markdown_oxide", "pyright" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
