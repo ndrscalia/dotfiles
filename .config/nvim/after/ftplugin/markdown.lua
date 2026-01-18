d77350e11c1e28bb54f44180a82e375fbb06b0eb
ndrscalia <andrea95scalia@gmail.com>
2026-01-18 21:17:15 +0100

    add quick toggle for telescope docs symbols

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,8 +1,8 @@
--- Spell checking (default English)
+-- spell checking (default English)
 vim.opt_local.spell = true
 vim.opt_local.spelllang = "en"
 
--- Word count
+-- word count
 vim.keymap.set("n", "<leader>wc", function()
   local wc = vim.fn.wordcount()
   print("Words: " .. wc.words .. " | Chars: " .. wc.chars)
@@ -10,3 +10,6 @@ end, { buffer = true, desc = "Word count" })
 
 -- renderer-markdown quick toggle
 vim.keymap.set("n", "<leader>mr", ":RenderMarkdown toggle<CR>", { buffer = true, desc = "Toggle render markdown" })
+
+-- browse md sections quick toggle
+vim.keymap.set("n", "<leader>ms", ":Telescope lsp_document_symbols<CR>", { buffer = true, desc = "Markdown sections" })
